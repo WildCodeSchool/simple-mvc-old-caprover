f117186d61afc9e5e372194ef89d7b66b4e65731
poutine63 <barbier.aurelien@gmail.com>
2017-10-25 10:27:54 +0200

    Redirect to true URL if medias requested

--- public/index.php
+++ public/index.php
@@ -6,6 +6,11 @@
  * Time: 14:01
  */
 
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // retourne la requête telle quelle.
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/db.php';
 require_once __DIR__ . '/../app/config.php';
