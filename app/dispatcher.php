3d596f4f5860e14b956eeda9ff6ed549486e6228
Aurelien <aurelien@wildcodeschool.fr>
2018-03-22 17:11:00 +0100

    Resolve include issue

--- app/dispatcher.php
+++ app/dispatcher.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/routing.php';
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) use ($routes) {
   foreach ($routes as $controller => $actions) {
     foreach ($actions as $action) {
-        $r->addRoute($action[2], $action[1], $controller.'/' . $action[0]);      
+        $r->addRoute($action[2], $action[1], $controller.'/' . $action[0]);
     }
   }
 });
@@ -30,10 +30,12 @@ $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
         // ... 404 Not Found
+        header("HTTP/1.0 404 Not Found");
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = $routeInfo[1];
         // ... 405 Method Not Allowed
+        header("HTTP/1.0 405 Method Not Allowed");
         break;
     case FastRoute\Dispatcher::FOUND:
         $handler = $routeInfo[1];
--- app/routing.php
+++ app/routing.php
@@ -3,6 +3,8 @@
 $routes = [
   'Item' => [ // Controller
     ['index', '/', 'GET'], // action, url, method
+    ['add', '/item/add', 'GET'], // action, url, method
+    ['edit', '/item/edit/{id:\d+}', 'GET'], // action, url, method
     ['show', '/item/{id:\d+}', 'GET'], // action, url, method
   ]
 ];
