a3778253ddbf176aab7c91284167148b5eaf2f15
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2018-08-21 16:23:04 +0200

    quete 1 simple-mvc

--- public/index.php
+++ public/index.php
@@ -1,17 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 11/10/17
- * Time: 14:01
- */
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <title>Simple MVC - Home</title>
 
-if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
-    return false;    // retourne la requête telle quelle.
-}
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="icon" href="/assets/images/favicon.png">
+</head>
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../app/db.php';
-require_once __DIR__ . '/../app/config.php';
-require_once __DIR__ . '/../app/dispatcher.php';
+<body>
+    <section>
+        <h1>Bienvenue sur le Simple MVC</h1>
+        <a href="item.php">Voir les items</a>
+    </section>
+</body>
+</html>
