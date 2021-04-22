735cf6eca04c6d0f08c11b657349f591ab745cee
Vincent Vaur <vaur.vincent@gmail.com>
2021-04-22 14:27:31 +0200

    Remove query string when router is guessing the controller's method's name to call.
    Set default controller's method to "index" instead of "" when guessing method's name.

--- src/routing.php
+++ src/routing.php
@@ -10,11 +10,23 @@
  * @link     https://github.com/WildCodeSchool/simple-mvc
  */
 
-$routeParts = explode('/', ltrim($_SERVER['REQUEST_URI'], '/') ?: HOME_PAGE);
+// Removes the querystring from the requested URL so "/myMethod?q=var" becomes "/myMethod".
+$requestedUrl = preg_replace('/\?.*/', '', ltrim($_SERVER['REQUEST_URI'], '/')) ?: HOME_PAGE;
+
+// Split the URL in fragments
+$routeParts = explode('/', $requestedUrl);
+
+// Building the fully qualified controller classname depending on the first URL fragment
 $controller = 'App\Controller\\' . ucfirst($routeParts[0] ?? '') . 'Controller';
-$method = $routeParts[1] ?? '';
+
+// Guessing the method called depending on the second URL fragment. Set index by default.
+$method = $routeParts[1] ?? 'index';
+
+// Extract additionnals parameters from additionals URL fragments
 $vars = array_slice($routeParts, 2);
 
+// If the guessed method exists on the guessed controller, call the method.
+// Otherwise returns a 404 error.
 if (class_exists($controller) && method_exists(new $controller(), $method)) {
     echo (new $controller())->$method(...$vars);
 } else {
