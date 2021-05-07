e338b261c7d21e0f5074f940ee1acf774b836622
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-05-07 16:12:12 +0200

    update constant name

--- config/config.php
+++ config/config.php
@@ -14,10 +14,10 @@
 define('ENV', getenv('ENV') ? getenv('ENV') : 'dev');
 
 //Model (for connexion data, see unversionned db.php)
-define('DB_USER', getenv('DB_USER') ? getenv('DB_USER') : APP_DB_USER);
-define('DB_PASSWORD', getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : APP_DB_PASSWORD);
-define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : APP_DB_HOST);
-define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : APP_DB_NAME);
+define('DB_USER', getenv('DB_USER') ? getenv('DB_USER') : DB_USER);
+define('DB_PASSWORD', getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : DB_PASSWORD);
+define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : DB_HOST);
+define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : DB_NAME);
 
 //VIew
 define('APP_VIEW_PATH', __DIR__ . '/../src/View/');
