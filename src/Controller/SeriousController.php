4e984e28d6c56b916f2c6b51e92d3a9af69cb627
Léa Mahler <leamahler85@gmail.com>
2021-05-12 01:18:19 +0200

    fixed sniffer

--- src/Controller/SeriousController.php
+++ src/Controller/SeriousController.php
@@ -8,10 +8,6 @@ class SeriousController extends AbstractController
 {
     public function startSerious()
     {
-        // Get the images from the API but do really need it ???????
-        $client = HttpClient::create();
-        // $response = $client->request('GET', 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY');
-        // $results = $response->toArray();
         return $this->twig->render('Serious/serious-start.html.twig');
     }
 
