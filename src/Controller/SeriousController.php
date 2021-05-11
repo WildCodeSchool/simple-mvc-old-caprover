af17414141c6df435ee6f0c6ec556b12899e6801
Léa Mahler <leamahler85@gmail.com>
2021-05-11 23:13:56 +0200

    iaozeiazhoazihe

--- src/Controller/SeriousController.php
+++ src/Controller/SeriousController.php
@@ -8,11 +8,12 @@ class SeriousController extends AbstractController
 {
     public function startSerious()
     {
+        // Récupérer l'API, est-ce qu'on s'en sert ???
         $client = HttpClient::create();
-        $response = $client->request('GET', 'https://pokeapi.co/api/v2/pokemon/51');
-        $imageSerious1 = $response->toArray();
-        var_dump($imageSerious1);
-        return $this->twig->render('Serious/serious-start.html.twig');
+        $imageSerious1 = $client->request('GET', 'https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00060/opgs/edr/fcam/FRA_402820707EDR_F0050104FHAZ00202M_.JPG');
+        return $this->twig->render('Serious/serious-start.html.twig', [
+            'imageSerious1' => $imageSerious1
+        ]);
     }
 
     public function endSerious()
