904be1905ca447e5ad375fc6f962691ce1393d90
Léa Mahler <leamahler85@gmail.com>
2021-05-12 11:18:16 +0200

    renamed serious controllers

--- src/Controller/SeriousController.php
+++ src/Controller/SeriousController.php
@@ -6,12 +6,12 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class SeriousController extends AbstractController
 {
-    public function startSerious()
+    public function seriousStart()
     {
         return $this->twig->render('Serious/serious-start.html.twig');
     }
 
-    public function endSerious()
+    public function seriousEnd()
     {
         return $this->twig->render('Serious/serious-end.html.twig');
     }
