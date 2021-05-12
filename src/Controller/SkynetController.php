28afbccfc7044f11fd5913988331dec9ec8893a2
Léa Mahler <leamahler85@gmail.com>
2021-05-12 10:41:15 +0200

    okokok

--- src/Controller/SkynetController.php
+++ src/Controller/SkynetController.php
@@ -7,14 +7,18 @@ class SkynetController extends AbstractController
     public function skynetStart()
     {
         session_start();
-        return $this->twig->render('/Skynet/skynet-start.html.twig',
-        ['name' => $_SESSION['name']]);
+        return $this->twig->render(
+            '/Skynet/skynet-start.html.twig',
+            ['name' => $_SESSION['name']]
+        );
     }
 
     public function skynetEnd()
     {
         session_start();
-        return $this->twig->render('/Skynet/skynet-end.html.twig',
-            ['name' => $_SESSION['name']]);
+        return $this->twig->render(
+            '/Skynet/skynet-end.html.twig',
+            ['name' => $_SESSION['name']]
+        );
     }
 }
