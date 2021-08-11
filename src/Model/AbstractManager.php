2ec118f957dad809eb5406f500ffbb141d706a38
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-08-11 17:45:54 +0200

    add update and create to AbstractManager

--- src/Model/AbstractManager.php
+++ src/Model/AbstractManager.php
@@ -56,6 +56,49 @@ abstract class AbstractManager
     }
 
     /**
+     * Insert new item in database
+     */
+    public function insert(array $item): int
+    {
+        $fieldNames = [];
+        $fieldValues = [];
+        $preparedValues = [];
+        foreach ($item as $key => $value) {
+            if ($key != 'id') {
+                $fieldNames[] = "`$key`";
+                $fieldValues[] = ":$key";
+                $preparedValues[":$key"] = $value;
+            }
+        }
+        $fieldNamesFlat = implode(',', $fieldNames);
+        $fieldValuesFlat = implode(',', $fieldValues);
+        $statement = $this->pdo->prepare("INSERT INTO " . static::TABLE .
+            " ($fieldNamesFlat) VALUES ($fieldValuesFlat)");
+
+        $statement->execute($preparedValues);
+        return (int)$this->pdo->lastInsertId();
+    }
+
+    /**
+     * Update item in database
+     */
+    public function update(array $item): bool
+    {
+        $fieldNames = [];
+        $preparedValues = [];
+        foreach ($item as $key => $value) {
+            if ($key != 'id') {
+                $fieldNames[] = "`$key` = :$key";
+            }
+            $preparedValues[":$key"] = $value;
+        }
+        $fieldNamesFlat = implode(',', $fieldNames);
+        $statement = $this->pdo->prepare("UPDATE " . static::TABLE . " SET $fieldNamesFlat WHERE id=:id");
+
+        return $statement->execute($preparedValues);
+    }
+
+    /**
      * Delete row form an ID
      */
     public function delete(int $id): void
