9a8e9e68c1d1eccc75abdab9df38fd2b796ef949
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-11-05 15:58:00 +0100

    fix migration

--- migration.php
+++ migration.php
@@ -1,20 +1,23 @@
 <?php
 
-use App\Model\Connection;
-
 require 'vendor/autoload.php';
 if (file_exists('config/db.php')) {
     require 'config/db.php';
-} else {
-    require 'config/db.php.dist';
 }
-
 
 require 'config/config.php';
 
 try {
-    $dbConnection = new Connection();
-    $pdo = $dbConnection->getPdoConnection();
+    $pdo = new PDO(
+        'mysql:host=' . DB_HOST . '; charset=utf8',
+        DB_USER,
+        DB_PASSWORD
+    );
+
+    $pdo->exec('DROP DATABASE IF EXISTS ' . DB_NAME);
+    $pdo->exec('CREATE DATABASE ' . DB_NAME);
+    $pdo->exec('USE ' . DB_NAME);
+
     if (is_file(DB_DUMP_PATH) && is_readable(DB_DUMP_PATH)) {
         $sql = file_get_contents(DB_DUMP_PATH);
         $statement = $pdo->prepare($sql);
