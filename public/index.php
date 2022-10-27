db0c97b3ec9ccad02d80e7da96e28348de2cf736
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2022-10-27 17:22:06 +0200

    fix deploy caprover

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../config/debug.php';
-require_once __DIR__ . '/../config/db.php';
-
+if (file_exists(__DIR__ . '/../config/db.php')) {
+    require_once __DIR__ . '/../config/db.php';
+}
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
